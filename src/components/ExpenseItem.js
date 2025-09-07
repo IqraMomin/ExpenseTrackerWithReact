b191c0b96c9a8c3d07463df20ae5d3174c299455
IqraMomin <iqra2018momin@gmail.com>
2025-09-07 20:32:45 +0530

    StylingComponents

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,14 @@
+import './ExpenseItem.css';
+
 function ExpenseItem(){
     return(
         <div>
-            <div>7 September 2025</div>
-            <div>
+            <div className='expense-item'>7 September 2025</div>
+            <div className='expense-item__description'>
                 <h2>Book</h2>
-                <div>$10</div>
+                <div className='expense-item__price'>$10</div>
                 <h2>Location</h2>
-                <div>Delhi</div>
+                <div className='expense-item__location'>Delhi</div>
             </div>
         </div>
     )
