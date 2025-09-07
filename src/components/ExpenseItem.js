5d23f128ac5520cd432987d048f9c7de9a3a17f7
IqraMomin <iqra2018momin@gmail.com>
2025-09-08 00:02:01 +0530

    JavascriptInJSX

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,18 @@
 import './ExpenseItem.css';
 
 function ExpenseItem(){
+    const expenseDate = new Date(2023,7,15).toISOString();
+    const expenseTitle = "Book";
+    const expensePrice = "10";
+    const expenseLocation = "Bangalore";
     return(
         <div>
-            <div className='expense-item'>7 September 2025</div>
+            <div className='expense-item'>{expenseDate}</div>
             <div className='expense-item__description'>
-                <h2>Book</h2>
-                <div className='expense-item__price'>$10</div>
+                <h2>{expenseTitle}</h2>
+                <div className='expense-item__price'>${expensePrice}</div>
                 <h2>Location</h2>
-                <div className='expense-item__location'>Delhi</div>
+                <div className='expense-item__location'>{expenseLocation}</div>
             </div>
         </div>
     )
