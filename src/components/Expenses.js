f03f4c23add908b0c0e929a91132a211bcbe9ba1
IqraMomin <iqra2018momin@gmail.com>
2025-09-10 03:20:34 +0530

    WrapperComponents

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,10 +1,10 @@
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
+import Card from "./Card"
 
 function Expenses(props) {
-    console.log(props);
     return (
-        <div className="expenses">
+        <Card className="expenses">
             {props.expenses.map((expense, index) => {
                 return (
                     <ExpenseItem
@@ -16,7 +16,7 @@ function Expenses(props) {
                     ></ExpenseItem>
                 )
             })}
-        </div>
+        </Card>
     )
 }
 
