043da54f28d51cc94a7efac39522b7e1f372162f
IqraMomin <iqra2018momin@gmail.com>
2025-09-13 22:55:07 +0530

    ConditionalRendering

--- src/components/Expense/Expenses.js
+++ src/components/Expense/Expenses.js
@@ -12,21 +12,31 @@ function Expenses(props) {
     const filteredByYear = props.expenses.filter(expense => {
         return (expense.date.getFullYear().toString() === filteredYear)
     })
+    let expenseContent = <h3 style={{color:"gray"}}>No expenses found</h3>
+    const filteredContent = filteredByYear.map((expense, index) => {
+        return (
+            <ExpenseItem
+                key={expense.id}
+                title={expense.title}
+                date={expense.date}
+                amount={expense.amount}
+                location={expense.location}
+            />
+        )
+    });
+    if (filteredByYear.length === 1) {
+        expenseContent = <div>
+            {filteredContent}
+            <h3 style={{color:"gray"}}>Only one expense found. Please add more</h3>
+        </div>
+    } else if (filteredByYear > 1) {
+        expenseContent = filteredContent;
+    }
     return (
         <div>
             <Card className="expenses">
-            <ExpensesFilter onFilterChange={filterChangeHandler} selected={filteredYear} />
-                {filteredByYear.map((expense, index) => {
-                    return (
-                        <ExpenseItem
-                            key={expense.id}
-                            title={expense.title}
-                            date={expense.date}
-                            amount={expense.amount}
-                            location={expense.location}
-                        />
-                    )
-                })}
+                <ExpensesFilter onFilterChange={filterChangeHandler} selected={filteredYear} />
+                {expenseContent}
             </Card>
         </div>
     )
